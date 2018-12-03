7ae196a3369c29884795b334c4027ea1f89caf71
ciprian9 <c15341751@mydit.ie>
2018-12-03 02:04:16 +0000

    Delete from playlist

---
+++ MyApplication4/app/src/main/java/com/example/nicholasanton/myapplication/My_Delete_Playlist_Adapter.java
@@ -0,0 +1,80 @@
+package com.example.nicholasanton.myapplication;
+
+/*
+Code coming from
+https://stackoverflow.com/questions/17525886/listview-with-add-and-delete-buttons-in-each-row-in-android
+ */
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class My_Delete_Playlist_Adapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+    private DataHandler db;
+    private String selectedItem;
+
+
+    public My_Delete_Playlist_Adapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+        db = new DataHandler(context);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return list.indexOf(list.get(pos));
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.delete_list_row, null);
+        }
+
+
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        Button deleteBtn = (Button)view.findViewById(R.id.delete_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                selectedItem = (String) getItem(position);
+                list.remove(position); //or some other task
+                db.DeletePlaylists(selectedItem);
+                notifyDataSetChanged();
+            }
+        });
+
+        return view;
+    }
+}
+
---
+++ MyApplication4/app/src/main/java/com/example/nicholasanton/myapplication/Playlist_View.java
@@ -0,0 +1,49 @@
+package com.example.nicholasanton.myapplication;
+
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Playlist_View extends AppCompatActivity {
+
+    private ArrayList<String> list;
+    DataHandler db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist__view);
+        list = new ArrayList<>();
+        //generate list
+        SetItems();
+        db = new DataHandler(this);
+
+        //code from
+        //https://stackoverflow.com/questions/17525886/listview-with-add-and-delete-buttons-in-each-row-in-android
+        //instantiate custom adapter
+        My_Delete_Playlist_Adapter adapter = new My_Delete_Playlist_Adapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView)findViewById(R.id.delete_list);
+        lView.setAdapter(adapter);
+    }
+
+    private void SetItems(){
+        list.clear();
+        DataHandler db = new DataHandler(this);
+        Cursor c = db.SelectPlaylistQuery(1);
+        if (c.moveToFirst()){
+            for(int i=0; i<c.getCount(); i++){
+                String name = c.getString(Constants.COLUMN_PLAYLIST_NAME);
+                list.add(name);
+                c.moveToNext();
+            }
+        }
+    }
+}
